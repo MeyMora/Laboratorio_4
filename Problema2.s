83b0604399745e3b1208ec3ca8d248f7af39d3ed
MeyMora <112515882+MeyMora@users.noreply.github.com>
2025-05-20 21:30:07 -0600

    Update Problema2.s

--- Problema2.s
+++ Problema2.s
@@ -1,14 +1,15 @@
     .section .data
-X:      .word 3   @ Aquí definimos X (puedes cambiar este valor para probar)
+X:      .word 5   @ Aquí definimos X 
 result: .word 1   @ Variable para almacenar el resultado del factorial
 
     .section .text
     .global _start
 
 _start:
-    LDR R0, =X       @ Cargar dirección de X
-    LDR R1, [R0]     @ Cargar valor de X en R1
-    MOV R2, #1       @ Inicializar resultado (factorial = 1)
+	LDR R0, =X       @ Cargar dirección de X
+	LDR R1, [R0]     @ Cargar valor de X en R1
+	MOV R2, #1       @ Inicializar resultado (factorial = 1)
+	MOV R4, R1       @ Guardar copia de X en R4 antes de modificar R1
 
 factorial_loop:
     CMP R1, #1       @ Si X <= 1, salir del bucle
@@ -23,5 +24,3 @@ end_factorial:
     STR R2, [R3]     @ Almacenar el resultado del factorial
     MOV R7, #1       @ Código de salida
     SVC 0            @ Terminar ejecución
-
-	
